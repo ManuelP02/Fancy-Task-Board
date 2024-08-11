c9d84bb7e0c7122aa0083a6f1599905c5e2ee00d
ManuelP02 <manuelpena0207@gmail.com>
2024-08-10 21:23:01 -0400

    Added js file and readme

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Fancy-Task-Board
+# Fancy Task Board
+## Description
+This project is a simple task board application designed to help project teams manage tasks efficiently. The application runs in the browser and features dynamically updated HTML and CSS powered by jQuery. It utilizes the Day.js library to handle date-related functionalities. The task board allows users to add, manage, and track the progress of project tasks.
+## User Story
+As a project team member with multiple tasks to organize, I want a task board so that I can add individual project tasks, manage their state of progress, and track overall project progress accordingly.
+## Acceptance Criteria 
+- Task Display: When the task board is opened, the list of project tasks is displayed in columns representing the task progress states: Not Yet Started, In Progress, and Completed.
+
+- Color Coding: Each task is color-coded to indicate its deadline status: yellow for nearing the deadline and red for overdue.
+
+- Task Creation: Users can define a new task by entering the title, description, and deadline date in a modal dialog.
+
+- Task Saving: Upon clicking the save button, the task properties are saved in localStorage.
+
+- Task Drag and Drop: Tasks can be dragged to different progress columns, updating their progress state, which persists after refreshing.
+
+- Task Deletion: Clicking the delete button removes a task from the board, and it will not reappear after refreshing.
+
+- Data Persistence: Saved tasks persist even after the page is refreshed.
+
+## Features
+
+1- Dynamic Task Management: Add, edit, and delete tasks with ease.
+
+2- Drag-and-Drop Interface: Intuitive drag-and-drop functionality to update task progress.
+
+3- Deadline Alerts: Visual alerts for tasks nearing or past their deadlines.
+
+4- Local Storage: Persistent data storage using localStorage.
+
+## Technical Requirements
+
+* Day.js Library: Utilized for handling date-related operations.
+
+* jQuery: Powers dynamic updates to HTML and CSS.
+
+## Demo
+
+### Images
+#### Index
+![index](https://github.com/user-attachments/assets/9da5136a-45f5-4f97-9150-34f623a5d0c0)
+#### Add Task Modal
+![addtask](https://github.com/user-attachments/assets/92049c5e-d80a-4bdb-ad91-a59afb87749a)
+#### Moving Task
+![movingTask](https://github.com/user-attachments/assets/1a8065d6-727f-4b75-bda8-fe0cdc375819)
+
+
+### Deployed App
+https://manuelp02.github.io/Fancy-Task-Board/
+
+
+## License
+This project did not required a license.
+
+## Contact
+If questions, contact the developer at: 
+
+m.e.penafernandez@gmail.com
+
+
+
+
+ 
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,164 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+let tasks = [];
+
+function compareDates(dueDate) {
+  const formattedDueDate = dayjs(dueDate);
+
+  if (formattedDueDate.isTomorrow() || formattedDueDate.isToday()) {
+    return { cardBg: 'bg-warning', btnborder: null };
+  }
+  if (formattedDueDate.isSameOrBefore()) {
+    return { cardBg: 'bg-danger text-white', btnBorder: 'border-white' };
+  }
+  return { cardBg: null, btnBorder: null };
+}
+
+// Generate a unique task id
+function generateTaskId() {
+  return Math.floor(Math.random() * 100000).toString();
+}
+
+// Create a task card
+function createTaskCard({ id, taskTitle, taskDueDate, taskDescription, status }, isDone) {
+  let statusClass = '';
+  if (status === 'to-do') {
+    statusClass = 'bg-primary'; // Blue for To Do if it's not due tomorrow or today
+  } else if (status === 'in-progress') {
+    statusClass = 'bg-success'; // Green for In Progress if it's not due tomorrow or today
+  }
+
+  const newTaskCard = $(
+    `<div class='card task-card draggable ${statusClass} ${!isDone && compareDates(taskDueDate).cardBg}'
+    data-task='${id}'>`
+  );
+
+  newTaskCard.html(`<h4 class='card-header'>${taskTitle}</h4>
+    <div class='card-body'>
+      <p>${taskDescription}</p>
+      <p>${dayjs(taskDueDate).format('MM/DD/YYYY')}</p>
+      <button class='btn btn-danger' ${!isDone && compareDates(taskDueDate).btnBorder}'>Delete</button>
+    </div>
+    `);
+
+  newTaskCard.find('button').on('click', handleDeleteTask);
+  return newTaskCard;
+}
+
+// Render the task list and make cards draggable
+function renderTaskList() {
+  $('#todo-cards').html('');
+  $('#in-progress-cards').html('');
+  $('#done-cards').html('');
+
+  tasks.forEach((task) => {
+    if (task.status === 'to-do') {
+      $('#todo-cards').append(createTaskCard(task, false));
+    }
+    if (task.status === 'in-progress') {
+      $('#in-progress-cards').append(createTaskCard(task, false));
+    }
+    if (task.status === 'done') {
+      $('#done-cards').append(createTaskCard(task, true));
+    }
+  });
+
+  $('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    helper: function (e) {
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
+}
+
+// Handle adding a new task
+function handleAddTask(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  $(form).addClass('was-validated');
+
+  if (!form.checkValidity()) {
+    return;
+  }
+
+  const taskTitle = $(event.target).find('input')[0].value;
+  const taskDueDate = $(event.target).find('input')[1].value;
+  const taskDescription = $(event.target).find('input')[2].value;
+  const newTask = {
+    id: generateTaskId(),
+    taskTitle,
+    taskDueDate,
+    taskDescription,
+    status: 'to-do',
+  };
+
+  tasks.push(newTask);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  // Clear input fields
+  $(event.target).find('input').val('');
+
+  // Alert the user
+  alert('Task has been created successfully!');
+
+  // Refresh the page
+  location.reload();
+}
+
+// Handle deleting a task
+function handleDeleteTask(event) {
+  const deleteCardId = $(event.target).parent().parent()[0].dataset.task;
+  tasks = tasks.filter((task) => {
+    return task.id !== deleteCardId;
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTaskList();
+}
+
+// Handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+  const taskId = ui.draggable[0].dataset.task;
+
+  const newStatus = event.target.id;
+
+  tasks.forEach((task) => {
+    if (task.id === taskId) {
+      task.status = newStatus;
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTaskList();
+}
+
+// When the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+  $('#taskForm').on('submit', handleAddTask);
+
+  $(function () {
+    $('.lane').droppable({
+      accept: '.draggable',
+      drop: handleDrop,
+    });
+  });
+
+  if (!taskList) {
+    tasks = [];
+    return;
+  }
+
+  tasks = taskList;
+  renderTaskList();
+});
